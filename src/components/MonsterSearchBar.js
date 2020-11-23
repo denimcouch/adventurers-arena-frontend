637232e4410608eeb45a7ccb374319593f749c22
denimcouch <alexmata92@ymail.com>
2020-11-23 10:32:53 -0600

    can filter by size and type

--- src/components/MonsterSearchBar.js
+++ src/components/MonsterSearchBar.js
@@ -1,13 +1,68 @@
 import React from "react";
-import MonstersContainer from "../containers/MonstersContainer";
-import { Dropdown } from "semantic-ui-react";
-
-const sizeOptions = [];
 
 function MonsterSearchBar(props) {
-  let { searchMonsters } = props
+  let {
+    setSearchTerm,
+    setSizeFilter,
+    setTypeFilter,
+    sizeOptions,
+    alignmentOptions,
+    crOptions,
+    typeOptions,
+  } = props;
+
   return (
     <div id="search-bar-container">
+      <div className="filters">
+        <label>
+          <strong>Filter by Size: </strong>
+          <select onChange={(e) => setSizeFilter(e.target.value)}>
+            <option value="">All</option>
+            {sizeOptions.map((size) => (
+              <option value={size}>{size}</option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <label>
+          <strong>Filter by Type: </strong>
+          <select onChange={(e) => setTypeFilter(e.target.value)}>
+            <option value="">All</option>
+            {typeOptions.map((type) => (
+              <option value={type}>{type}</option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <label>
+          <strong>Filter by Alignment: </strong>
+          <select onChange={(e) => console.log(e.target.value)}>
+            <option value="">All</option>
+            {alignmentOptions.map((align) => (
+              <option value={align}>{align}</option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <label>
+          <strong>Min CR: </strong>
+          <select onChange={(e) => console.log(e.target.value)}>
+            <option value="">All</option>
+            {crOptions.map((cr) => (
+              <option value={cr}>{cr}</option>
+            ))}
+          </select>
+        </label>{" "}
+        <label>
+          <strong>Max CR: </strong>
+          <select onChange={(e) => console.log(e.target.value)}>
+            <option value="">All</option>
+            {crOptions.map((cr) => (
+              <option value={cr}>{cr}</option>
+            ))}
+          </select>
+        </label>
+      </div>
 
       <label>
         <strong>Search</strong>
@@ -15,13 +70,12 @@ function MonsterSearchBar(props) {
           <div className="ui icon input">
             <input
               className="prompt"
-              onChange={(e) => searchMonsters(e.target.value)}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
             <i className="search icon" />
           </div>
         </div>
       </label>
-
     </div>
   );
 }
--- src/containers/MonstersContainer.js
+++ src/containers/MonstersContainer.js
@@ -1,26 +1,96 @@
 import React, { Component } from "react";
-import MonsterTable from '../components/MonsterTable'
-import MonsterSearchBar from '../components/MonsterSearchBar'
+import MonsterTable from "../components/MonsterTable";
+import MonsterSearchBar from "../components/MonsterSearchBar";
 
 class MonstersContainer extends Component {
   state = {
     searchTerm: "",
-    filterBySize: "",
-    filterByType: "",
+    sizeFilter: "",
+    typeFilter: "",
+    alignFilter: "",
     minCR: "",
     maxCR: "",
   };
 
-  searchMonsters = string => {
-    this.setState({searchTerm: string})
-  }
+  //updating state with the following functions
+  setSearchTerm = (string) => {
+    this.setState({ searchTerm: string });
+  };
+  setSizeFilter = (sizeFilter) => {
+    this.setState({
+      sizeFilter,
+    });
+  };
+  setTypeFilter = (typeFilter) => {
+    this.setState({ typeFilter });
+  };
+
+  //uses searchTerm to find Monsters
+  searchMonsters = () => {
+    return this.props.monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+    );
+  };
+
+  // creates array of size categories
+  sizeOptions = () => {
+    let sizeArray = [];
+    this.props.monsters.map((monster) => {
+      return sizeArray.includes(monster.size)
+        ? null
+        : sizeArray.push(monster.size);
+    });
+    return sizeArray;
+  };
+  // creates array of alignment categories
+  alignmentOptions = () => {
+    let alignmentArray = [];
+    this.props.monsters.map((monster) => {
+      return alignmentArray.includes(monster.alignment)
+        ? null
+        : alignmentArray.push(monster.alignment);
+    });
+    return alignmentArray.sort((a, b) => a - b);
+  };
+  // creates array of challenge ratings
+  crOptions = () => {
+    let crArray = [];
+    this.props.monsters.map((monster) => {
+      return crArray.includes(monster.challenge_rating)
+        ? null
+        : crArray.push(monster.challenge_rating);
+    });
+    return crArray.sort((a, b) => a - b);
+  };
+  // creates array of types
+  typeOptions = () => {
+    let typeArray = [];
+    this.props.monsters.map((monster) => {
+      return typeArray.includes(monster.mon_type)
+        ? null
+        : typeArray.push(monster.mon_type);
+    });
+    return typeArray;
+  };
 
   render() {
-    let monsters = this.props.monsters.filter( monster => monster.name.toLowerCase().includes(this.state.searchTerm.toLocaleLowerCase()))
+    let monsters = this.searchMonsters().filter(
+      (monster) =>
+        monster.size.includes(this.state.sizeFilter) &&
+        monster.mon_type.includes(this.state.typeFilter)
+    );
     return (
       <div>
         <h2>Monsters Container</h2>
-        <MonsterSearchBar searchMonsters={this.searchMonsters} />
+        <MonsterSearchBar
+          setSearchTerm={this.setSearchTerm}
+          setSizeFilter={this.setSizeFilter}
+          setTypeFilter={this.setTypeFilter}
+          sizeOptions={this.sizeOptions()}
+          alignmentOptions={this.alignmentOptions()}
+          crOptions={this.crOptions()}
+          typeOptions={this.typeOptions()}
+        />
         <MonsterTable monsters={monsters} />
       </div>
     );
