5ee906be78963b845758e76b15523ecd42ef810b
denimcouch <alexmata92@ymail.com>
2020-11-25 12:01:22 -0600

    minor styling to manage encounters

--- src/containers/EncountersContainer.js
+++ src/containers/EncountersContainer.js
@@ -11,26 +11,30 @@ function EncountersContainer(props) {
       <h1>{user.username}'s Encounters</h1>
       {user.encounters.map((encounter) => {
         return (
-          <div className="user-encounter-container">
-            <h2 className='user-encounter-title'>{encounter.name}</h2>
-            <div className="user-encounter-info">
-              {encounter.monsters.map((mon) => {
-                return (
-                  <div className="user-monster-info">
-                    <Monster monster={mon} />
-                    <span>CR: {mon.challenge_rating}</span>{" "}
-                    <span>EXP: {formatEXP(mon.exp)}</span>
-                    <br />
-                  </div>
-                );
-              })}
+          <>
+            <div className="user-encounter-container">
+              <h2 className="user-encounter-title">{encounter.name}</h2>
+              <div className="user-encounter-info">
+                {encounter.monsters.map((mon) => {
+                  return (
+                    <div className="user-monster-info">
+                      <Monster monster={mon} />
+                      <span>CR: {mon.challenge_rating}</span>{" "}
+                      <span>EXP: {formatEXP(mon.exp)}</span>
+                      <br />
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="user-encounter-actions">
+                <Divider />
+                <Button primary>Edit Encounter</Button>
+                <Button negative>Delete Encounter</Button>
+              </div>
             </div>
-            <div className="user-encounter-actions">
-              <Divider />
-              <Button primary>Edit Encounter</Button>
-              <Button negative>Delete Encounter</Button>
-            </div>
-          </div>
+            <br />
+            <Divider />
+          </>
         );
       })}
     </div>
