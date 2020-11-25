20ad48b86fb8686984e7898f0324611b9313f68a
denimcouch <alexmata92@ymail.com>
2020-11-25 11:55:36 -0600

    structure of user encounters created

--- src/containers/EncountersContainer.js
+++ src/containers/EncountersContainer.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import EncounterForm from '../components/EncounterForm'
+import React from "react";
+import Monster from "../components/Monster";
+import { formatEXP } from "../components/MonsterTable";
+import { Button, Divider } from "semantic-ui-react";
 
 function EncountersContainer(props) {
-
-  return(
+  let { user } = props;
+  console.log(user.encounters);
+  return (
     <div>
-      <h1>Encounters Container</h1>
+      <h1>{user.username}'s Encounters</h1>
+      {user.encounters.map((encounter) => {
+        return (
+          <div className="user-encounter-container">
+            <h2 className='user-encounter-title'>{encounter.name}</h2>
+            <div className="user-encounter-info">
+              {encounter.monsters.map((mon) => {
+                return (
+                  <div className="user-monster-info">
+                    <Monster monster={mon} />
+                    <span>CR: {mon.challenge_rating}</span>{" "}
+                    <span>EXP: {formatEXP(mon.exp)}</span>
+                    <br />
+                  </div>
+                );
+              })}
+            </div>
+            <div className="user-encounter-actions">
+              <Divider />
+              <Button primary>Edit Encounter</Button>
+              <Button negative>Delete Encounter</Button>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default EncountersContainer
+export default EncountersContainer;
