dab4a155f04141e7192b9efae6ab46b9ad5edacb
denimcouch <alexmata92@ymail.com>
2020-12-02 10:56:20 -0600

    monster modal styled

--- src/components/Monster.js
+++ src/components/Monster.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Modal, Icon, Divider } from "semantic-ui-react";
+import { Button, Modal, Icon } from "semantic-ui-react";
 
 function monsterReducer(state, action) {
   switch (action.type) {
@@ -20,6 +20,29 @@ function Monster({ monster, xp }) {
   const { open, dimmer } = state;
 
   const formatData = (array) => array.join(" | ");
+  const modalStyle = {
+    background:'#FDF1DC',
+    color:'black',
+  }
+  const modalHeader = {
+    fontSize: '36px',
+    background:'#FDF1DC',
+    color:'black',
+    borderBottom: '5px solid rgb(172, 2, 2)'
+  }
+  const modalFooter = {
+    fontSize: '36px',
+    background:'#FDF1DC',
+    color:'black',
+    borderStyle: 'hidden'
+  }
+  const modalBackground = {
+    borderStyle: "ridge",
+    borderWidth: "7px",
+    borderColor: "orange",
+    borderRadius: "5px",
+    boxShadow: "10px 10px 5px black"
+  }
 
   return (
     <div>
@@ -33,57 +56,71 @@ function Monster({ monster, xp }) {
         dimmer={dimmer}
         open={open}
         onClose={() => dispatch({ type: "CLOSE_MODAL" })}
+        style={modalBackground}
       >
-        <Modal.Header>{monster.name}</Modal.Header>
-        <Modal.Content>
-          <div>
-            <p>
-              <b>Armor Class</b> {monster.armor_class}
-              <br />
-              <b>Hit Points</b> {monster.hit_points}({monster.hit_dice})
-              <br />
-              <b>Speed</b> {formatData(monster.speed)}
-            </p>
-          </div>
-          <Divider />
-          <div>
-            <p>
-              STR: {monster.strength} | DEX: {monster.dexterity} | CON:{" "}
-              {monster.constitution} | INT: {monster.intelligence} | WIS:{" "}
-              {monster.wisdom} | CHA: {monster.charisma}
-            </p>
-          </div>
-          <Divider />
-          <div>
-            <p>
-              <b>Damage Vulnerabilities</b>{" "}
-              {monster.damage_vulnerable.length === 0
-                ? "none"
-                : formatData(monster.damage_vulnerable)}
-              <br />
-              <b>Damage Resistances</b>{" "}
-              {monster.damage_resist.length === 0
-                ? "none"
-                : formatData(monster.damage_resist)}
-              <br />
-              <b>Damage Immunities</b>{" "}
-              {monster.damage_immune.length === 0
-                ? "none"
-                : formatData(monster.damage_immune)}
-              <br />
-              <b>Senses</b> {formatData(monster.senses)}
-              <br />
-              <b>Languages</b> {monster.languages}
-              <br />
-              <b>Challenge</b> {monster.challenge_rating} ({xp} exp)
-            </p>
-          </div>
-          <Divider />
+        <Modal.Header style={modalHeader} className="stat-block-header">
+          {monster.name}
+        </Modal.Header>
+        <Modal.Content style={modalStyle}>
+          <div className="stat-block">
+            <div className="stat-block-info">
+              <p>
+                <b>Armor Class</b> {monster.armor_class}
+                <br />
+                <b>Hit Points</b> {monster.hit_points}({monster.hit_dice})
+                <br />
+                <b>Speed</b> {formatData(monster.speed)}
+              </p>
+            </div>
+            <br/>
+            <div className="stat-block-info">
+              <p className="stat-block-abilities">
+                STR: {monster.strength} | DEX: {monster.dexterity} | CON:{" "}
+                {monster.constitution} | INT: {monster.intelligence} | WIS:{" "}
+                {monster.wisdom} | CHA: {monster.charisma}
+              </p>
+            </div>
+            <br/>
+            <div className="stat-block-info">
+              <p>
+                <strong>
+                  Damage Vulnerabilities:{" "}
+                </strong>{" "}
+                {monster.damage_vulnerable.length === 0
+                  ? "none"
+                  : formatData(monster.damage_vulnerable)}
+                <br />
+                <strong>
+                  Damage Resistances:{" "}
+                </strong>{" "}
+                {monster.damage_resist.length === 0
+                  ? "none"
+                  : formatData(monster.damage_resist)}
+                <br />
+                <strong>
+                  Damage Immunities:{" "}
+                </strong>{" "}
+                {monster.damage_immune.length === 0
+                  ? "none"
+                  : formatData(monster.damage_immune)}
+                <br />
+                <strong>Senses: </strong>{" "}
+                {formatData(monster.senses)}
+                <br />
+                <strong>Languages: </strong>{" "}
+                {monster.languages}
+                <br />
+                <strong>Challenge</strong>{" "}
+                <strong>{monster.challenge_rating} ({xp} exp)</strong>
+              </p>
+            </div>
+            {/* <Divider />
           Special Abilities go here
           <Divider />
-          Actions go here
+          Actions go here */}
+          </div>
         </Modal.Content>
-        <Modal.Actions>
+        <Modal.Actions style={modalFooter}>
           <Button
             icon
             negative
